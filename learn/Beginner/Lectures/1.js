87a39edf20a46375e79d9ab3f1a30e4065df4ecf
aboamen7000 <aboamen7000@gmail.com>
2023-04-26 06:29:01 +0200

    Beginner

---
+++ learn/Beginner/Lectures/1.js
@@ -0,0 +1,11 @@
+// const name = 'IA';
+const name = process.argv[2];
+
+if (name === 'IA')
+{
+    console.log(`Hello Ibrahim you loged as ${name}`);
+}
+else
+{
+    console.log(`Hello Stranger, ${name}`);
+}
